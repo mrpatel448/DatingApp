757f2884e25e3271d56530477a0ff72c9b391ced
Shubham <shubham.aakashinfo@gmail.com>
2023-01-05 16:25:23 +0530

    End of section 7

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -5,8 +5,10 @@ namespace API.DTOs
     public class RegisterDTO
     {
         [Required]
-        public string UserName { get; set; }        
+        public string UserName { get; set; }
+
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
